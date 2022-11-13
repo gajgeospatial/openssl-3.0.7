fd8cd6da098fc84700f07f85dc03a4595cf2359c
Glen A Johnson <glen.johnson@gaj-geospatial.com>
2022-11-12 23:45:35 -0500

    Initial checkin

---
+++ Build_OpenSSL_with_vs2019_Command_Window.bat
@@ -0,0 +1,6 @@
+Perl Configure VC-WIN64A --debug
+nmake
+call MoveDebug_vs2019.bat
+Perl Configure VC-WIN64A
+nmake
+call MoveRelease_vs2019.bat
---
+++ Build_OpenSSL_with_vs2022_Command_Window.bat
@@ -0,0 +1,6 @@
+Perl Configure VC-WIN64A --debug
+nmake
+call MoveDebug.bat
+Perl Configure VC-WIN64A
+nmake
+call MoveRelease.bat
